0c909210e507350ddd7d2544f9e502504c989585
Farhan Raza <razafarhan967@gmail.com>
2025-06-12 21:05:34 +0500

    Create Character replacing.cpp
    Write a program that accepts input from the user and creates another string that contains either a u, l, or - for each character of the original string. Use u when the character is uppercase, and use l when the character is lowercase. If the character is neither uppercase nor lowercase, use -. Print the original string first, then print the modified string.

---
+++ Character replacing.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int main(){
+  
+  string original;
+getline(cin, original);
+  string modified;
+  char ch;
+for (int i = 0; i < original.length(); i++) {
+    ch = original[i];
+    if (isupper(ch)) {
+      modified += 'u';
+    }
+    else if (islower(ch)) {
+      modified += 'l';
+    } 
+      else {
+      modified += '-';
+    }
+  }
+
+  cout << original << endl;
+  cout << modified << endl;
+
+  return 0;
+  
+}
