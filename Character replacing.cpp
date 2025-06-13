bc6980a74fc78e8bd9e66e76cc113c02c43a4d62
Farhan Raza <razafarhan967@gmail.com>
2025-06-13 20:43:27 +0500

    Update Character replacing.cpp

--- Character replacing.cpp
+++ Character replacing.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 using namespace std;
 int main(){
-  
   string original;
 getline(cin, original);
   string modified;
@@ -18,10 +17,7 @@ for (int i = 0; i < original.length(); i++) {
       modified += '-';
     }
   }
-
   cout << original << endl;
   cout << modified << endl;
-
   return 0;
-  
 }
